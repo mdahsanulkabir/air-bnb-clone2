8c1189d3fbf8e21a60aba299ea3189acf67f646e
ahsan <makabir98@yahoo.com>
2023-07-31 08:18:38 +0600

    some cleanup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({ subsets: ['latin'] })
 
 export const metadata = {
