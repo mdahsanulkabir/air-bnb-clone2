6de8f8d6d859c5e7fb84501a210325f8e1a637d0
ahsan <makabir98@yahoo.com>
2023-07-31 09:16:30 +0600

    testing tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export const metadata = {
 export default function RootLayout({children,}: { children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <div className='text-rose-500 text-2xl'>
         Hello
       </div>
-      
-    </main>
   )
 }
