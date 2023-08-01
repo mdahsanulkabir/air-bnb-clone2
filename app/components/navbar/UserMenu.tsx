ad9076178f17f253770346f5d9d906e4d3b46f54
ahsan <makabir98@yahoo.com>
2023-08-02 01:43:57 +0600

    Feature: Navbar/UserMenu partially done

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,15 @@
 'use client';
-//?
+// useState used, so 'use client'
 
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleOpen = useCallback( () => {
+        setIsOpen((value) => !value);
+    }, [])
     return (
         <div
             className="relative flex flex-row items-center gap-3"
@@ -16,7 +21,7 @@ const UserMenu = () => {
                 Airbnb your home
             </div>
             <div
-                onClick={() => {}}
+                onClick={toggleOpen}
                 className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
             >
                 <AiOutlineMenu />
@@ -24,6 +29,17 @@ const UserMenu = () => {
                     <Avatar />
                 </div>
             </div>
+            {
+                isOpen && (
+                    <div
+                        className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'
+                    >
+                        <div className='flex flex-col cursor-pointer'>
+                            Menu Item
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
