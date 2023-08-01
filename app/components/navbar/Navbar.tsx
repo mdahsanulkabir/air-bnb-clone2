527e6f582d23b3ca5514cb85f9fc96cc4a694762
ahsan <makabir98@yahoo.com>
2023-08-02 01:28:21 +0600

    Feature: Navbar/Search partially done

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
@@ -12,7 +13,7 @@ const Navbar = () => {
                 >
                     <Logo />
                     <Search />
-                    
+                    <UserMenu />
                 </div>
             </Container>
         </div>
