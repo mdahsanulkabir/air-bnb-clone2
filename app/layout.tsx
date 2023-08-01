e4d033d3d4144406d452d4b4f59ce09d3ee3bfe5
ahsan <makabir98@yahoo.com>
2023-08-02 03:47:33 +0600

    Feature: Register > RegisterModal partially done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import Modal from './components/modals/Modal'
+
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 // import { Inter } from 'next/font/google'
@@ -16,7 +17,7 @@ export default function RootLayout({children,}: { children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal actionLabel='Submit' isOpen title="Hello World"/>
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
